266307e5ee6070779a076563613514878f11b7b7
jakusz1 <13085272+jakusz1@users.noreply.github.com>
2018-01-24 11:30:54 +0100

    tests added

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get topics_index_url
-    assert_response :success
+  test "should not add topic with no data" do
+  	topic=Topic.new
+  	assert_not topic.save
+  end
+  test "should not add topic with bad number of attending students" do
+  	topic=Topic.new(attending_number: 12, title: "test", studies_type: "magisterskie", if_eng: true, teacher_id: 1)
+  	assert_not topic.save
+  end
+  test "should add topic" do
+  	topic=Topic.new(attending_number: 1, title: "test_dobry", studies_type: "magisterskie", if_eng: true, teacher_id: 1)
+  	assert_not topic.save
   end
 
 end
